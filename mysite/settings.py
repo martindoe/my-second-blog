a30bc60e8682ee93f947b7335da155276ec113c7
martindoe <36802467+martindoe@users.noreply.github.com>
2018-08-07 16:58:41 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1@0(qqz9ow6(w1(&nc@!vglqfsqips8)g$b6_vh!c&)d8p@pb3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'warrior112.pythonanywhere.com']
 
